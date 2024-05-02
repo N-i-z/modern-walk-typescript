f3bb90839889549f2545e30a7bd0b3c4ee06b9b1
Nisma Nizam <fnismanizam@gmail.com>
2024-05-02 21:30:40 +0530

    feat: add homepage content

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import { Home } from "./pages/home/home";
 import { MensClothing } from "./pages/men's clothing/men's clothing";
 import { WomensClothing } from "./pages/women's clothing/women's clothing";
 
+const MensClothingCategory = () => {
+  return <MensClothing />;
+};
+
+const WomensClothingCategory = () => {
+  return <WomensClothing />;
+};
+
 function App() {
   return (
     <div className="App">
@@ -12,8 +20,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/men's clothing" element={<MensClothing />} />
-          <Route path="/women's clothing" element={<WomensClothing />} />
+          <Route path="/men's clothing" element={<MensClothingCategory />} />
+          <Route
+            path="/women's clothing"
+            element={<WomensClothingCategory />}
+          />
         </Routes>
       </Router>
     </div>
