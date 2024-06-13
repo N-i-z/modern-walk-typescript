666f0ebf9abcda7a2f10435daea62961ae2f5bb2
Nisma Nizam <fnismanizam@gmail.com>
2024-06-13 12:23:34 +0530

    chore: Comment out error causing code

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import reportWebVitals from "./reportWebVitals.tsx";
+// import reportWebVitals from "./reportWebVitals.tsx";
 
 const rootElement = document.getElementById("root");
 
@@ -14,7 +14,7 @@ if (rootElement) {
     </React.StrictMode>
   );
 
-  reportWebVitals();
+  // reportWebVitals();
 } else {
   console.error(
     'Root element not found. Please check your HTML file for an element with id="root".'
