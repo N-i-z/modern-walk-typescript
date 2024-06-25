3fd4dfc4bc2a1ac0050829397e958485240fc9c2
Nisma Nizam <fnismanizam@gmail.com>
2024-06-25 12:29:44 +0530

    refactor: Refactor fetch calls using react query

---
+++ .changeset/bright-knives-eat.md
@@ -0,0 +1,5 @@
+---
+"modern-walk": minor
+---
+
+- Integrate TanStack Query to project
--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { Home, WatchList } from "./ui-core/templates";
 import { MensClothing } from "./ui-core/templates";
 import { WomensClothing } from "./ui-core/templates";
 import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const MensClothingCategory: React.FC = () => {
   return <MensClothing />;
 };
@@ -17,32 +18,38 @@ const WomensClothingCategory: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/mens-clothing" element={<MensClothingCategory />} />
-          <Route path="/womens-clothing" element={<WomensClothingCategory />} />
-          <Route
-            path="/watchlist"
-            element={
-              <SignedIn>
-                <WatchList />
-              </SignedIn>
-            }
-          />{" "}
-          <Route
-            path="*"
-            element={
-              <SignedOut>
-                <RedirectToSignIn />
-              </SignedOut>
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
+    <QueryClientProvider client={new QueryClient()}>
+      <ReactQueryDevtools />
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mens-clothing" element={<MensClothingCategory />} />
+            <Route
+              path="/womens-clothing"
+              element={<WomensClothingCategory />}
+            />
+            <Route
+              path="/watchlist"
+              element={
+                <SignedIn>
+                  <WatchList />
+                </SignedIn>
+              }
+            />{" "}
+            <Route
+              path="*"
+              element={
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+    </QueryClientProvider>
   );
 };
 
