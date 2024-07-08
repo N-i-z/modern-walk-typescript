8caad9c6907a84c1d882b0a60057bc8a43402dab
Nisma Nizam <fnismanizam@gmail.com>
2024-07-08 21:03:11 +0530

    refactor: Fix product card paragraph style

--- src/ui-core/components/molecules/ProductCard/ProductCard.component.tsx
+++ src/ui-core/components/molecules/ProductCard/ProductCard.component.tsx
@@ -12,7 +12,7 @@ const ProductCard = ({
   descriptionBackgroundColor,
 }: ProductCardProps) => {
   return (
-    <div className="card bg-white rounded-[50px] w-[420px] h-[550px] max-w-[800px] m-2 mb-12 flex flex-col justify-between items-center shadow-lg overflow-hidden">
+    <div className="card bg-white rounded-[50px] w-[420px] h-[550px] max-w-[800px] m-2 mb-12 flex flex-col justify-between items-center shadow-lg overflow-hidden relative">
       <div className="card-title max-w-xs mx-12 mt-6 text-center truncate">
         <h3 className="text-2xl text-black font-bold truncate">{title}</h3>
       </div>
@@ -24,15 +24,15 @@ const ProductCard = ({
         />
       </div>
       <div
-        className={`card-description w-full h-[250px] p-5 rounded-t-[40px] flex flex-col items-center`}
+        className={`card-description w-full h-[250px] p-5 rounded-t-[40px] flex flex-col items-center relative`}
         style={{ backgroundColor: descriptionBackgroundColor }}
       >
         <h4 className="text-bluep font-bold text-3xl m-0">{`Rs ${price}`}</h4>
         <br />
-        <p className="text-[16px] leading-6 text-center line-clamp-[2] mx-8">
+        <p className="text-[16px] leading-6 text-center line-clamp-3 mx-8">
           {description}
         </p>
-        <div className="button-container flex justify-center items-center gap-4 mt-4">
+        <div className="button-container flex justify-center items-center gap-4 absolute bottom-2">
           <WatchlistButton
             itemId={title}
             itemName={title}
